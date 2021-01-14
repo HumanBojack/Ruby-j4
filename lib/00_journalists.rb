7fe0966c5e29d91173ad7bf73bc822778cb0577b
HumanBojack <spychalar59@gmail.com>
2021-01-14 19:03:47 +0100

    Make everything beautiful and functionnal

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -1,5 +1,126 @@
 account = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
 
+#1 nombre de handles
+def account_size(account)
+return account.size
+end
+
+#2 Plus petit
+def smallest(account)
+min = 100
+valeur = ""
+account.each do |i|
+	if i.size < min
+		min = i.size
+		valeur = i
+	end
+end
+return valeur , min
+end
+
+#3 5 caractères
+def five_characters_accounts(account)
+length5 = []
+value = ""
+account.each do |i|
+	if i.size.to_i == 6
+		length5.push i
+	end
+end
+return length5
+end
+
+#4 Majuscule
+def uppercase(account)
+arr = [*("A".."Z")]
+count = 0
+account.each do |i|
+	if arr.include? i.slice(1,1)
+	count += 1
+	end
+end
+return count
+end
+
+#5 Trie AZ
+def sort_alpha(account)
+return account.sort
+end
+
+#6 Trie taille
+def sort_size(account)
+sorted = []
+34.times do |j|
+account.each do |i|
+	if i.size == j
+		sorted.push i
+	end
+end
+end
+return sorted
+end
+
+#7 trouve epenser
+def epenser(account)
+value = ""
+count = 1
+account.each do |i|
+	if i == "@epenser"
+		value = count
+	end
+	count += 1
+end
+return value
+end
+
+#8 nb caractères
+def characters_by_element(account)
+first = []
+second = []
+34.times do |j|
+	count = 0 
+	account.each do |i|
+	if i.size == j
+		count += 1
+		first.push[j] = "Nombre de comptes comprenant #{j} caractères : #{count}"
+	end
+end
+end
+first.each do |i|
+	unless i == nil
+	second.push i
+end
+end
+return second
+end
+
+while true
+print "1-Handle par array / 2-Handle le plus court / 3-Handle de 5 caractères / 4-Majuscule / 5-Triez de A à Z / 6-Triez par taille / 7-Position epenser / 8-Répartition Handle par taille"
+puts
+nb = gets.chomp.to_i
+system("clear")
+puts "-" * 20
+if nb == 1
+	puts "Il y a #{account_size(account)} comptes."
+elsif nb == 2
+	puts "Le compte avec le moins de caractères est #{smallest(account)[0]} avec #{smallest(account)[1]-1} caractères (@ non compris)."
+elsif nb == 3
+	puts "Voici une liste des comptes faisant 5 caractères :", five_characters_accounts(account)
+elsif nb == 4
+	puts "Il y a #{uppercase(account)} comptes comprenant une lettre majuscule comme premier caractère"
+elsif nb == 5
+	puts "Voici la liste de comptes triée par ordre alphabétique :", sort_alpha(account)
+elsif nb == 6
+	puts "Voici la liste de comptes triée par taille :", sort_size(account)
+elsif nb == 7
+	puts "Le compte @epenser se retrouve à la position n°#{epenser(account)}"
+elsif nb == 8
+	puts characters_by_element(account)
+else 
+	exit
+end
+puts "-" * 20
+end
 
 
 
@@ -9,78 +130,3 @@ account = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@
 
 
 
-# nb caractères
-# test1 = []
-# test2 = []
-# 34.times do |j|
-# 	compteur = 0 
-# 	account.each do |i|
-# 	if i.size.to_i == j
-# 		compteur += 1
-# 		test1.push[j] = "Nombre d'éléments à #{j} caractères : #{compteur}"
-# 	end
-# end
-# end
-# test1.each do |i|
-# 	if i != nil
-# 	test2.push i
-# end
-# end
-# puts test2
-
-# trouve epenser
-# valeur = ""
-# compteur = 1
-# account.each do |i|
-# 	if i == "@epenser"
-# 		valeur = compteur
-# 	end
-# 	compteur += 1
-# end
-# puts valeur
-
-# Trie taille
-# test1 = []
-# 34.times do |j|
-# account.each do |i|
-# 	if i.size == j
-# 		test1.push i
-# 	end
-# end
-# end
-# puts test1
-
-# Trie AZ
-# comptetrie = account.sort
-# puts comptetrie
-
-# Majuscule
-# arr = [*("A".."Z")]
-# compteur = 0
-# account.each do |i|
-# 	if arr.include? i.slice(1,1)
-# 	compteur += 1
-# 	end
-# end
-# puts compteur
-
-# 5 caractères
-# length5 = []
-# valeur =""
-# account.each do |i|
-# 	if i.size.to_i == 6
-# 		length5.push i
-# 	end
-# end
-# puts length5
-
-# plus petit
-# min = 100
-# valeur =""
-# account.each do |i|
-# 	if i.size.to_i < min
-# 		min = i.size
-# 		valeur = i
-# 	end
-# end
-# puts valeur
